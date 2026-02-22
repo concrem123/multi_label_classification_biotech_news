2b022a6bf04920af5e6d8e52349e3a32dfa77146
concrem123 <conor.m.cremin@gmail.com>
2026-02-22 20:29:45 +0000

    Added data preprocessing

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+Setup
+python3 -m venv .venv 
+source .venv/bin/activate 
+pip install -r requirements.txt
+
 # multi_label_classification_biotech_news
 This model uses a fine tuned pubmed bert model to label biotech news. Each biotech news article can have multiple labels
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import os
-import numpy as np
 from sklearn.model_selection import train_test_split
 import yaml
 from pathlib import Path
-from datasets import load_dataset, DatasetDict
+from datasets import Dataset, DatasetDict
+from transformers import DistilBertTokenizer
 
 def preprocess_and_save(cfg):
     train_df = pd.read_csv(
@@ -17,8 +17,16 @@ def preprocess_and_save(cfg):
 
     all_data = pd.concat([train_df, test_df], ignore_index=True)
 
-    train_df['Text'] = "Title: "+ train_df['Title'] + "Content: " +train_df['Content'] + "Target Organization: " + train_df['Target Organization']
-    test_df['Text'] = "Title: "+ test_df['Title'] + "Content: " +test_df['Content'] + "Target Organization: " + test_df['Target Organization']
+    for df in [train_df, test_df]:
+        df["Title"] = df["Title"].fillna("").astype(str)
+        df["Content"] = df["Content"].fillna("").astype(str)
+        df["Target Organization"] = df["Target Organization"].fillna("").astype(str)
+
+        df["Text"] = (
+            "Title: " + df["Title"]
+            + " Content: " + df["Content"]
+            + " Target Organization: " + df["Target Organization"]
+        )
 
     eval_split = cfg['data']['eval_split']
     train_df, eval_df = train_test_split(
@@ -33,10 +41,6 @@ def preprocess_and_save(cfg):
     # Create a new DataFrame containing only the selected label columns
     df_labels_train = train_df[label_columns]
     df_labels_test = test_df[label_columns]
-
-    # Convert the label columns to lists for each row
-    labels_list_train = df_labels_train.values.tolist()
-    labels_list_test = df_labels_test.values.tolist()
 
     unique_list = []
     for i, label in enumerate(label_columns):
@@ -54,13 +58,45 @@ def preprocess_and_save(cfg):
         eval_df[label] = eval_df[label].map(mapping_values)
 
     dataset_dict = DatasetDict({
-        "train": train_df,
-        "test": test_df,
-        "eval": eval_df})
+    "train": Dataset.from_pandas(train_df, preserve_index=False),
+    "test": Dataset.from_pandas(test_df, preserve_index=False),
+    "eval": Dataset.from_pandas(eval_df, preserve_index=False),
+    })
+    
+    tokenizer = DistilBertTokenizer.from_pretrained(cfg["model"]["name"], do_lower_case=True)
+    
+
+    # Tokenization function
+    def preprocess_function(examples):
+        return tokenizer(
+            examples[cfg["data"]["text_column"]],
+            truncation=True,
+            padding="max_length",
+            max_length=cfg["model"]["max_length"],
+        )
+
+    # Apply tokenization to all splits
+    encoded_with_text = dataset_dict.map(preprocess_function, batched=True, desc="Tokenizing")
+
+    # Resolve project root and dataset paths
+    ROOT = Path(__file__).resolve().parent.parent
+    processed_path = (ROOT / cfg["data"]["processed_path"]).resolve()
+    processed_debug_path = (ROOT / cfg["data"]["processed_path_debug"]).resolve()
+
+    # Create directories if they don't exist
+    processed_debug_path.parent.mkdir(parents=True, exist_ok=True)
+    processed_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Save dataset with text (for debugging)
+    encoded_with_text.save_to_disk(str(processed_debug_path))
+
+    # Remove text column for training 
+    encoded_dataset = encoded_with_text.remove_columns([cfg["data"]["text_column"]],'Title', 'Content', 'Target Organization')
+    encoded_dataset.save_to_disk(str(processed_path))
 
 if __name__ == "__main__":
     ROOT = Path(__file__).resolve().parent.parent
-    config_path = ROOT / "configs/configs.yaml"
+    config_path = ROOT / "config/config.yaml"
 
     with open(config_path, "r") as f:
         cfg = yaml.safe_load(f)
